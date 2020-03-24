220ddf9b4b6bc713310e167e6627d8bb98faf85d
Yliku <370495310@qq.com>
2020-03-24 19:40:17 +0800

    广播系统 News事件类：修改频道为 public 公有的，谁都可以收听到；添加漏掉的 message属性

--- app/Events/News.php
+++ app/Events/News.php
@@ -13,6 +13,7 @@ use Illuminate\Queue\SerializesModels;
 class News implements ShouldBroadcast {     //继承 ShouldBroadcast 广播类
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
+    public $message;
     /**
      * Create a new event instance.
      *
@@ -28,6 +29,8 @@ class News implements ShouldBroadcast {     //继承 ShouldBroadcast 广播类
      * @return \Illuminate\Broadcasting\Channel|array
      */
     public function broadcastOn() {
-        return new PrivateChannel('news');  //在哪个频道进行广播
+        return new Channel('news');            //在公共频道进行广播，谁都可以收听的广播
+        // return new PrivateChannel('news');  //在私有频道进行广播，只有指定用户可以收到的广播
+        // return new PresenceChannel('news'); //不仅可以收听到跟你有关的广播，还可以跟别的用户互动，适合做聊天室
     }
 }
